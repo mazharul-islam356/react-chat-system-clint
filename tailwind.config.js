67e9a29e97fcd73256a30e8ea582b442c982fc2c
rifat3568 <mazharulislam3569@gmail.com>
2024-08-19 19:29:39 +0600

    add register and login pagee

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
+/* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
-export default {
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+module.exports = withMT({
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -7,7 +10,6 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [
-    require('daisyui'),
-  ],
-}
+  plugins: [require("daisyui")],
+});
+
